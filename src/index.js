ba5b940539247b5cb671153eec30b9f9a4aba296
Anatolij Dejneko <anatoliideineko@gmail.com>
2021-08-18 15:47:19 +0100

    add persister for Token in authReducer

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store";
-// import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDom.render(
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
