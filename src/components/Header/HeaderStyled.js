d64a81f06afa9048e6958441340cd648c75a4569
Tofik Hasanov <annexcies@gmail.com>
2021-08-18 09:52:55 +0300

    fix header

---
+++ src/components/Header/HeaderStyled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  .menuNavImg {
+    width: 20px;
+    height: 20px;
+    fill: red;
+    /* background-color: red;  */
+  }
+`;
+
+export default HeaderStyled;
