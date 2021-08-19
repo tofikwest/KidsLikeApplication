7c7715c569c33faf734bce43f24f61699701d913
tatidan <t.danyliuk@gmail.com>
2021-08-19 12:37:41 +0300

    mainstyled

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,19 @@
 import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import { mainRoutes } from "../../routes/mainRoutes";
+import MainStyled from "./MainStyled";
 
 const Main = () => {
   return (
-    <Suspense fallback={<p>...loading</p>}>
-      <Switch>
-        {mainRoutes.map(({ path, exact, component }) => (
-          <Route path={path} exact={exact} component={component} key={path} />
-        ))}
-      </Switch>
-    </Suspense>
+    <MainStyled>
+      <Suspense fallback={<p>...loading</p>}>
+        <Switch>
+          {mainRoutes.map(({ path, exact, component }) => (
+            <Route path={path} exact={exact} component={component} key={path} />
+          ))}
+        </Switch>
+      </Suspense>
+    </MainStyled>
   );
 };
 
