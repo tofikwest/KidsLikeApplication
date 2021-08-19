f12a8107213f74797184f8f0ce4cdb9749edfbd1
trukozak <aleksandr.nepijvoda@gmail.com>
2021-08-19 12:49:56 +0300

    addLoader

---
+++ src/components/loader/LoaderStyled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LoaderStyled= styled.div`
+  position: fixed;
+
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100vw;
+  height: 100vh;
+  z-index: 999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+
+`
