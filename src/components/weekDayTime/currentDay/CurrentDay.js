f1c56c186320e59a876ae2b4878651ff003d30b0
trukozak <aleksandr.nepijvoda@gmail.com>
2021-08-23 00:04:46 +0300

    add_loader_fix_progresBar

--- src/components/weekDayTime/currentDay/CurrentDay.js
+++ src/components/weekDayTime/currentDay/CurrentDay.js
@@ -1,15 +1,21 @@
+import moment from "moment";
 import React from "react";
 import CurrentWeek from "../currentWeek/CurrentWeek";
 import { CurrentDayStyled } from "./CurrentDayStyled";
 
-const CurrentDay = ({ day, date }) => {
+let date = null;
+let day = null;
+
+const CurrentDay = () => {
+  date = moment().format("DD-MM-YYYY");
+  day = moment().format("dddd");
+  let newDay = day[0].toUpperCase() + day.slice(1);
   return (
     <CurrentDayStyled>
       <CurrentWeek />
       <span className="current-tasks">Мoи задачи:</span>
       <span className="current-day">
-        СРЕДА, 18-08-2021
-        {/* {`${day}`}, {`${date}`} */}
+        {`${newDay}`}, {`${date}`}
       </span>
     </CurrentDayStyled>
   );
