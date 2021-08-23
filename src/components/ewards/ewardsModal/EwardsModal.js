f143153da20b41ac684a3606920b57f5ca4a3d15
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-08-23 17:35:49 +0300

    fix style modalAwards

--- src/components/ewards/ewardsModal/EwardsModal.js
+++ src/components/ewards/ewardsModal/EwardsModal.js
@@ -4,8 +4,12 @@ import catImg from "../../../images/catModal.png";
 import EwardsModalStyled from "./EwardsModalStyled";
 import { colors } from "../../../general/styles/colors";
 import sprite from "../../../images/sprite.svg";
+import { useSelector } from "react-redux";
+import { getAwards } from "../../../redux/gifts/giftsSelectors";
 
 const EwardsModal = ({ setOpenModal }) => {
+  const awards = useSelector(getAwards);
+
   return (
     <EwardsModalStyled colors={colors}>
       <svg className="iconCloseAwards" onClick={setOpenModal}>
@@ -13,6 +17,19 @@ const EwardsModal = ({ setOpenModal }) => {
       </svg>
       <img className="catImg" src={catImg} alt="cat" />
       <h3 className="awardsModalTitle">Поздравляем! Ты получаешь:</h3>
+
+      <ul className="modalListAwards">
+        {awards.map((award) => (
+          <li className="modalListItemsAwards">
+            <img
+              className="modalListItemsImageAwards"
+              src={award.imageUrl}
+              alt={award.title}
+            />
+            <h4 className="modalListItemsTitleAwards">{award.title}</h4>
+          </li>
+        ))}
+      </ul>
     </EwardsModalStyled>
   );
 };
