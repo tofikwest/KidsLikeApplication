e1228a4763c3bcdc0816bff0b13c73205abd2e7a
Tofik Hasanov <annexcies@gmail.com>
2021-08-25 15:32:18 +0300

    fix deploy

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 const PublicRoutes = ({ isAuth, restricted, path, exact, component }) =>
-  isAuth && restricted ? <Redirect to="/" /> : <Route path={path} exact={exact} component={component} />;
+  isAuth && restricted ? (
+    <Redirect to="/" />
+  ) : (
+    <Route path={path} exact={exact} component={component} />
+  );
 
 export default PublicRoutes;
